5ce710f798e13eb8a8a423c4479dbe44e0b2236d
Wesley Pyburn <10319195+TCNOco@users.noreply.github.com>
2023-06-04 21:55:30 +0200

    Update roop.ps1

--- PowerShell/AI/roop.ps1
+++ PowerShell/AI/roop.ps1
@@ -164,8 +164,10 @@ if ($condaFound) {
     conda deactivate
     Update-SessionEnvironment
     #Open-Conda
+    conda info
     conda activate roop
-    conda install -c conda-forge -y
+    conda info
+    conda install mamba -c conda-forge -y
     python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
     python -m pip install -r requirements.txt
     conda install cudatoolkit -y
