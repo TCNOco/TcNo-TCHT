1bdea3e55dd30fdb6ac21d37da01d725ee47f929
Wesley Pyburn <10319195+TCNOco@users.noreply.github.com>
2024-09-08 18:09:54 +0200

    2024-09-08 - Windows Cleanup Script

---
+++ PowerShell/General/cleanup.ps1
@@ -0,0 +1,90 @@
+# Copyright (C) 2024 TroubleChute (Wesley Pyburn)
+# Licensed under the GNU General Public License v3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#    
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#    
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ----------------------------------------
+# This script will clear a ton of known cache/temp folders on Windows
+# A lot of these can be cleared without issue, but some require user
+# input as they may contain things like logins and more.
+# ----------------------------------------
+
+Write-Host "---------------------------------------------------------------------------" -ForegroundColor Cyan
+Write-Host "Welcome to TroubleChute's Cache & Temp Cleanup Tool!" -ForegroundColor Cyan
+Write-Host "This script:" -ForegroundColor Cyan
+Write-Host "- Automatically clear known Cache sources" -ForegroundColor Cyan
+Write-Host "- Prompt you when deleting more important Cache/Temp folders" -ForegroundColor Cyan
+Write-Host "[Version 2024-09-08]" -ForegroundColor Cyan
+Write-Host "`nThis script is provided AS-IS without warranty of any kind. See https://tc.ht/privacy & https://tc.ht/terms."
+Write-Host "Consider supporting these install scripts: https://tc.ht/support" -ForegroundColor Green
+Write-Host "---------------------------------------------------------------------------`n`n" -ForegroundColor Cyan
+
+Write-Host "NOTE: " -ForegroundColor Cyan -NoNewline
+Write-Host "When options include one capital letter, eg: (Y/n), the capital letter is the default option." -ForegroundColor Yellow
+Write-Host "Hitting Enter with nothing typed for (Y/n) will choose Yes. (y/N) will choose No.`n" -ForegroundColor Yellow
+
+iex (irm Import-RemoteFunction.tc.ht)
+Import-RemoteFunction("Get-GeneralFuncs.tc.ht")
+Import-FunctionIfNotExists -Command Get-FreeSpace -ScriptUri "File-Actions.tc.ht"
+
+Write-Host  "Space before cleanup: $(Get-FreeSpace)`n`n"
+
+function Confirm-Cleanup {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string[]]$Folders,
+        [string]$Text,
+        [bool]$DefaultYes = $true
+    )
+
+    Write-Host "Clear $Text? Locations include:"
+    foreach ($folder in $Folders) {
+        $expandedFolder = [Environment]::ExpandEnvironmentVariables($folder)
+        Write-Host "- $expandedFolder"
+    }
+        
+    if ($DefaultYes) {
+        do {
+            $continue = Read-Host "Continue? (Y/n)"
+        } while ($continue -notin "Y", "y", "1", "0", "N", "n", "")
+
+        if ($continue -in "Y", "y", "" ) {
+            Remove-Folders -Folders $Folders
+        } else {
+            Write-Host "Skippping."
+        }
+    } else {
+        do {
+            $continue = Read-Host "Continue? (y/N)"
+        } while ($continue -notin "Y", "y", "1", "0", "N", "n", "")
+        
+        if ($continue -in "Y", "y" ) {
+            Remove-Folders -Folders $Folders
+        } else {
+            Write-Host "Skippping."
+        }
+    }
+    
+}
+
+
+$Folders = @("C:\Windows\Temp", "C:\Temp", "C:\tmp", "C:\Windows\Prefetch", "$env:LocalAppData\Temp");
+Confirm-Cleanup -Text "known common temp/cache" -Folders $Folders
+
+
+
---
+++ PowerShell/Modules/General/File-Actions.psm1
@@ -0,0 +1,114 @@
+# Copyright (C) 2024 TroubleChute (Wesley Pyburn)
+# Licensed under the GNU General Public License v3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#    
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#    
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ----------------------------------------
+# This script has lots of useful commands for file/directory actions
+# ----------------------------------------
+
+<#
+.SYNOPSIS
+    Recursively deletes a folder and its contents.
+
+.DESCRIPTION
+    This function deletes a folder and all its contents, including subdirectories and files, recursively.
+    It includes an option to ignore any errors encountered during the deletion process.
+
+.PARAMETER Path
+    The full path of the folder to delete.
+
+.PARAMETER IgnoreErrors
+    If set to $true, the function will ignore any errors encountered during deletion.
+    Defaults to $true.
+
+.EXAMPLE
+    Remove-FolderRecursive -Path "C:\Temp\FolderToDelete"
+
+    This will delete the folder "C:\Temp\FolderToDelete" and all its contents.
+
+.EXAMPLE
+    Remove-FolderRecursive -Path "C:\Temp\FolderToDelete" -IgnoreErrors $true
+
+    This will delete the folder "C:\Temp\FolderToDelete", ignoring any errors encountered during deletion.
+#>
+
+function Remove-FolderRecursive {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Path,
+        [bool]$IgnoreErrors = $true
+    )
+
+    try {
+        if (Test-Path $Path) {
+            Remove-Item -Path $Path -Recurse -Force -ErrorAction Stop
+            Write-Host "Folder '$Path' deleted successfully."
+        } else {
+            Write-Host "Folder '$Path' does not exist."
+        }
+    } catch {
+        if ($IgnoreErrors) {
+            Write-Warning "An error occurred, but it was ignored: $_"
+        } else {
+            throw $_
+        }
+    }
+}
+
+<#
+.SYNOPSIS
+    Uses Remove-FolderRecursive on an array of folders.
+
+.PARAMETER Folders
+    Array of folder strings. Can include environment variables.
+
+.PARAMETER IgnoreErrors
+    If set to $true, the function will ignore any errors encountered during deletion.
+    Defaults to $true.
+
+#>
+function Remove-Folders {
+    param (
+        [Parameter(Mandatory = $false)]
+        [string[]]$Folders = $null,
+        [bool]$IgnoreErrors = $true
+    )
+
+    if (-not $Folders -or -not ($Folders -is [array])) {
+        Write-Host "No valid folders provided. Exiting."
+        return
+    }
+
+    foreach ($folder in $Folders) {
+        $expandedFolder = [Environment]::ExpandEnvironmentVariables($folder)
+        Write-Host "Processing folder: $expandedFolder"
+        
+        Remove-FolderRecursive -Path $expandedFolder -IgnoreErrors $IgnoreErrors
+    }
+}
+
+<#
+.SYNOPSIS
+    Returns string of free space on the system drive (Default C:\) in GB.
+#>
+function Get-FreeSpace {
+    $os = Get-WmiObject -Class Win32_OperatingSystem
+    $drive = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='$($os.SystemDrive)'" | Select @{Name="FreeGB";Expression={[math]::Round($_.FreeSpace / 1GB, 2)}}
+    return "$($drive.FreeGB) GB"
+}
